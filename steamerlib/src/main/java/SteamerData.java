ec16a56de3eeacebab362785512a87f02010247b
Dafna Arbiv <dafnaarbiv@172-13-2-32.lightspeed.mssnks.sbcglobal.net>
2016-03-10 16:23:11 +0200

    init android project

---
+++ steamerlib/src/main/java/SteamerData.java
@@ -0,0 +1,17 @@
+/**
+ * Created by dafnaarbiv on 10/03/16.
+ */
+public class SteamerData {
+    private static String test = "testinggggggggggg!!!! OMFG";
+
+    public SteamerData(){};
+    public SteamerData(String input)
+    {
+        test = input;
+    }
+
+    public String getTest()
+    {
+        return test;
+    }
+}
